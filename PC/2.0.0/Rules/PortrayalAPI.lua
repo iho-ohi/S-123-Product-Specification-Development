4390eb013bced0ea1f5ff4e3b54c3e8f207a5c10
Justin Scott <justin.scott@osimaritime.com>
2026-09-25 10:43:59 -0700

    Update to common S-100 Lua files
    Based on S-101 2.1

--- PC/2.0.0/Rules/PortrayalAPI.lua
+++ PC/2.0.0/Rules/PortrayalAPI.lua
@@ -30,11 +30,27 @@ function PortrayalCreateContextParameter(contextParameterName, parameterType, de
 	CheckType(contextParameterName, 'string')
 	CheckType(parameterType, 'string')
 
-	if parameterType ~= 'boolean' and parameterType ~= 'integer' and parameterType ~= 'real' and parameterType ~= 'text' and parameterType ~= 'date' then
-		error('Invalid parameter type.')
+	-- Case insensitive because the docs show leading capitalization but
+	-- ConvertEncodedValue requires lower, as does S100CD.xsd
+	local cpt = string.lower(parameterType)
+	
+	-- Allow types to be described per 9-13.3.30 ParameterType despite
+	-- 9a-14.1.3 requiring types to be described per S100_CD_AttributeValueType
+	if cpt == 'double' then
+		cpt = 'real'
+	elseif cpt == 'string' then
+		cpt = 'text'
+	end
+	
+	if cpt ~= 'boolean' and
+	   cpt ~= 'integer' and
+	   cpt ~= 'real' and
+	   cpt ~= 'text' and
+	   cpt ~= 'date' then
+		error('Invalid parameter type:' .. cpt)
 	end
 
-	return { Type = 'ContextParameter', Name = contextParameterName, ParameterType = parameterType, DefaultValue = ConvertEncodedValue(parameterType, defaultValue) }
+	return { Type = 'ContextParameter', Name = contextParameterName, ParameterType = cpt, DefaultValue = ConvertEncodedValue(cpt, defaultValue) }
 end
 
 function PortrayalSetContextParameter(contextParameterName, value)
@@ -111,7 +127,7 @@ local function LookupAttributeValue(container, attributeCode, HostGetSimpleAttri
 
 	--Debug.Break()
 
-	if not containerTypeInfo.AttributeBindings[attributeCode] then
+	if containerTypeInfo == nil or not containerTypeInfo.AttributeBindings[attributeCode] then
 		if nilIfMissing then
 			return nil
 		else
@@ -322,7 +338,7 @@ end
 --
 --
 
-function CreateAttributeConstraints(stringLength, textPattern, rangeLower, rangeUpper, closure, precision)
+function CreateAttributeConstraints(stringLength, textPattern, rangeLower, rangeUpper, rangeClosure, precision)
 	CheckTypeOrNil(stringLength, 'number')
 	CheckTypeOrNil(textPattern, 'string')
 	CheckTypeOrNil(rangeLower, 'string')
@@ -481,6 +497,7 @@ function CreateFeature(featureID, featureCode)
 		return feature
 	end
 	
+	-- Note: Potential namespace collision with attribute names. Consider renaming with leading underscore.
 	feature = { Type = 'Feature', ID = featureID, Code = featureCode, InformationAssociations = {}, FeatureAssociations = {} }
 
 	featureCache[featureID] = feature
@@ -497,7 +514,7 @@ function CreateFeature(featureID, featureCode)
 
 		if not ias then
 			Debug.StopPerformance('Lua Code - Total')
-			local informationIDs = HostFeatureGetAssociatedInformationIDs(self.ID, associationCode, roleCode)
+			local informationIDs = HostFeatureGetAssociatedInformationIDs(self.ID, associationCode, roleCode) or {}
 			Debug.StartPerformance('Lua Code - Total')
 
 			ias = {}
@@ -548,7 +565,7 @@ function CreateFeature(featureID, featureCode)
 
 		if not fas then
 			Debug.StopPerformance('Lua Code - Total')
-			local featureIDs = HostFeatureGetAssociatedFeatureIDs(self.ID, associationCode, roleCode)
+			local featureIDs = HostFeatureGetAssociatedFeatureIDs(self.ID, associationCode, roleCode) or {}
 			Debug.StartPerformance('Lua Code - Total')
 
 			fas = {}
@@ -626,10 +643,51 @@ function CreateFeature(featureID, featureCode)
 			return self['Spatial']
 		end
 	end
+	
+	function feature:FlattenSpatialAssociation(spas)
+		local fsa = {}
+		
+		local function FlattenCompositeCurve(compositeCurve)
+			for _, curveElement in ipairs(compositeCurve.CurveAssociations) do
+				if curveElement.SpatialType == SpatialType.CompositeCurve then
+					FlattenCompositeCurve(curveElement.Spatial)
+				else
+					fsa[#fsa + 1] = curveElement
+				end
+			end
+		end
+
+		local spatialType = spas.SpatialType
+
+		if spatialType == SpatialType.Point then
+			fsa[#fsa + 1] = spas
+		elseif spatialType == SpatialType.MultiPoint then
+			fsa[#fsa + 1] = spas
+		elseif spatialType == SpatialType.Curve then
+			fsa[#fsa + 1] = spas
+		elseif spatialType == SpatialType.CompositeCurve then
+			FlattenCompositeCurve(spas.Spatial)
+		elseif spatialType == SpatialType.Surface then
+			if spas.Spatial.ExteriorRing.SpatialType == SpatialType.CompositeCurve then
+				FlattenCompositeCurve(spas.Spatial.ExteriorRing.Spatial)
+			else
+				fsa[#fsa + 1] = spas.Spatial.ExteriorRing
+			end
+
+			for _, ring in ipairs(spas.Spatial.InteriorRings) do
+				if ring.SpatialType == SpatialType.CompositeCurve then
+					FlattenCompositeCurve(ring.Spatial)
+				else
+					fsa[#fsa + 1] = ring
+				end
+			end
+		end
+		
+		return fsa
+	end
 
 	-- Returns an iterator that returns all spatial associations to points, multi points and curves
 	-- associated to the feature.  Surface and composite curves return only their ultimate simple curves.
-	-- This only works for features with a single spatial association.
 	function feature:GetFlattenedSpatialAssociations()
 		local fsa = self['FlattenedSpatialAssociations']
 
@@ -638,35 +696,9 @@ function CreateFeature(featureID, featureCode)
 
 			fsa = self['FlattenedSpatialAssociations']
 
-			local function FlattenCompositeCurve(compositeCurve)
-				for _, curveElement in ipairs(compositeCurve.CurveAssociations) do
-					if curveElement.SpatialType == SpatialType.CompositeCurve then
-						FlattenCompositeCurve(curveElement.Spatial)
-					else
-						fsa[#fsa + 1] = curveElement
-					end
-				end
-			end
-
-			local spatialType = self:GetSpatialAssociation().SpatialType
-
-			if contains(spatialType, { SpatialType.Point, SpatialType.MultiPoint, SpatialType.Curve }) then
-				fsa[#fsa + 1] = self:GetSpatialAssociation()
-			elseif spatialType == SpatialType.CompositeCurve then
-				FlattenCompositeCurve(self.CompositeCurve)
-			elseif spatialType == SpatialType.Surface then
-				if self.Surface.ExteriorRing.SpatialType == SpatialType.CompositeCurve then
-					FlattenCompositeCurve(self.Surface.ExteriorRing.Spatial)
-				else
-					fsa[#fsa + 1] = self.Surface.ExteriorRing
-				end
-
-				for _, ring in ipairs(self.Surface.InteriorRings) do
-					if ring.SpatialType == SpatialType.CompositeCurve then
-						FlattenCompositeCurve(ring.Spatial)
-					else
-						fsa[#fsa + 1] = ring
-					end
+			for _, spas in ipairs(self:GetSpatialAssociations()) do
+				for _, v in ipairs(self:FlattenSpatialAssociation(spas)) do
+					fsa[#fsa + 1] = v
 				end
 			end
 		end
@@ -780,7 +812,7 @@ function CreateSpatialAssociation(spatialType, spatialID, orientation, scaleMini
 
 	function spatialAssociation:GetAssociatedFeatures()
 		Debug.StopPerformance('Lua Code - Total')
-		local featureIDs = HostSpatialGetAssociatedFeatureIDs(self.SpatialID)
+		local featureIDs = HostSpatialGetAssociatedFeatureIDs(self.SpatialID) or {}
 		Debug.StartPerformance('Lua Code - Total')
 
 		self.AssociatedFeatures = {}
@@ -805,7 +837,7 @@ function CreateSpatialAssociation(spatialType, spatialID, orientation, scaleMini
 
 		if not ias then
 			Debug.StopPerformance('Lua Code - Total')
-			local informationIDs = HostSpatialGetAssociatedInformationIDs(self.SpatialID, associationCode, roleCode)
+			local informationIDs = HostSpatialGetAssociatedInformationIDs(self.SpatialID, associationCode, roleCode) or {}
 			Debug.StartPerformance('Lua Code - Total')
 
 			ias = {}
@@ -867,7 +899,7 @@ local function CreateSpatial(spatialType, spatial)
 
 		if not ias then
 			Debug.StopPerformance('Lua Code - Total')
-			local informationIDs = HostSpatialGetAssociatedInformationIDs(self.SpatialID, associationCode, roleCode)
+			local informationIDs = HostSpatialGetAssociatedInformationIDs(self.SpatialID, associationCode, roleCode) or {}
 			Debug.StartPerformance('Lua Code - Total')
 
 			ias = {}
